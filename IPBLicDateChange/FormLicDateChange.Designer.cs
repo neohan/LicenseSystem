468ceb58f16eb1de5a91ddca9be4c2df56f3a2b2
neohan <neo_han@sohu.com>
2015-12-23 11:50:18 +0800

    增加一个修改智能号簿核心程序日期的程序。这样可以控制试用许可。

---
+++ IPBLicDateChange/FormLicDateChange.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace IPBLicDateChange
+{
+    partial class FmIPBLicDataChange
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(196, 12);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(12, 17);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 12);
+            this.labelResult.TabIndex = 1;
+            // 
+            // FmIPBLicDataChange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 46);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "FmIPBLicDataChange";
+            this.Text = "IPB License Date Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ IPBLicDateChange/FormLicDateChange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace IPBLicDateChange
+{
+    public partial class FmIPBLicDataChange : Form
+    {
+        public FmIPBLicDataChange()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            int pos = Application.ExecutablePath.LastIndexOf("\\");
+            string path = Application.ExecutablePath.Substring(0, pos);
+            string file = String.Format("Data Source={0}\\InteliPhoneBookService.exe", path);
+            DateTime now = DateTime.Now;
+            File.SetCreationTime(file, now);
+        }
+    }
+}
